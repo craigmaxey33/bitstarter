8d11f9c6519db01eb079ce3b6460e8a7df6f9ebf
craig maxey <craigmaxey33@mac.com>
2013-07-09 17:16:27 +0000

    try 2

--- web.js
+++ web.js
@@ -2,13 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var buffer = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html',"utf-8"));
 
-var out = buffer.toString("utf-8");
-
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(out);
+    response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
