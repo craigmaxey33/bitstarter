0e952eb4a74239655b70d6346ffba0f702ff6134
craig maxey <craigmaxey33@mac.com>
2013-07-09 16:53:17 +0000

    changed print for the buffer

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buffer.write(buffer.toString(fs.readFileSync('/home/ubuntu/bitstarter/index.html')),"utf-8"));
 });
 
 var port = process.env.PORT || 5000;
